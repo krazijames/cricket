8cd0db9b8ccd0cbc527c445fef577ab389d9ae13
krazijames <krazijames@gmail.com>
2019-11-13 00:00:10 +0900

    Display playlist name

--- src/pages/Playlist/Playlist.js
+++ src/pages/Playlist/Playlist.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import { useParams } from 'react-router-dom';
+import firebase from 'firebase/app';
 
 import { Page } from 'components';
+import { paths } from 'data';
 
 export default withStyles((theme) => ({}))(function Playlist({ classes }) {
   const { playlistId } = useParams();
-  return <Page>Playlist ID: {playlistId}</Page>;
+
+  const [playlist, setPlaylist] = React.useState();
+
+  React.useEffect(() => {
+    return firebase
+      .firestore()
+      .doc(`${paths.PLAYLISTS}/${playlistId}`)
+      .onSnapshot((doc) => {
+        setPlaylist({ id: doc.id, ...doc.data() });
+      });
+  }, [playlistId]);
+
+  return (
+    <Page title={playlist && playlist.name}>Playlist ID: {playlistId}</Page>
+  );
 });
